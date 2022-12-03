aeaf7b46999b96c0130208302fa9ff4da8f5f0ec
OnionPowder01 <autonatione36@gmail.com>
2022-12-03 12:04:55 +0200

    create random image picker

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,35 @@
 import React from "react";
+import { useState } from "react";
+import Data from '../memesData'
+
+
 
 export default function Meme() {
+    
+    function randomNumberInRange(min, max) {
+        
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+      
+    const URL = Data.data.memes[randomNumberInRange(0, 100)].url;
+    console.log(URL)
+
+    
+    const [isImageActive, setIsImageActive] = useState(false);
+        function clickEventHandler() {
+            setIsImageActive(true);
+        }
+    
+
     return (
         <main className="meme--container">
-            <form className="form">
+            <div className="form">
                
                 <input type="text" className="form--inputs" placeholder="Top text" />
                 <input type="text" className="form--inputs" placeholder="Bottom text" />
-                <button className="form--button">Get a new meme image 🖼</button>
-                </form>
+                <button className="form--button" onClick={clickEventHandler}>Get a new meme image 🖼</button>
+                {isImageActive && <img src={URL} alt="meme" />}
+                </div>
         </main>
     )
 }
